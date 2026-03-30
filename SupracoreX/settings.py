befc8910f5bda904aaf13e41b33507c9cbe527ce
rudrahariyani0157 <rudrahariyani0157@gmail.com>
2026-03-30 18:22:47 +0530

    settings.py

--- SupracoreX/settings.py
+++ SupracoreX/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3v8zng=%-zhprsr61i(z)wz!(o9*5xog@$2!pkm)egsyj+uj!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']     
 
 
 # Application definition
