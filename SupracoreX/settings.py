4e9f377b289d2caec81f3ede5b80f04b10340d25
Rudra Hariyani <rudrahariyani0157@gmail.com>
2026-06-06 13:08:04 +0530

    message

--- SupracoreX/settings.py
+++ SupracoreX/settings.py
@@ -141,3 +141,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
